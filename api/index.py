fdc5935e68d5f3e15d611dddc80f683780161924
ryancirish <ryancirish@users.noreply.github.com>
2023-03-11 23:20:19 -0500

    first

---
+++ api/index.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, request
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import os
+from flask_cors import CORS
+from datetime import datetime, date, timedelta
+import json
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # put in envar
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+CORS(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    email = db.Column(db.String())
+    hash = db.Column(db.String())
+    settings = db.Column(db.String())
+
+    def check_password(self, password):
+            return compare_digest(generate_password_hash(password), "hash")
+
+
+# Register a callback function that takes whatever object is passed in as the
+# identity when creating JWTs and converts it to a JSON serializable format.
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+# Register a callback function that loads a user from your database whenever
+# a protected route is accessed. This should return any python object on a
+# successful lookup, or None if the lookup failed for any reason (for example
+# if the user has been deleted from the database).
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+@app.route("/", methods=["GET"])
+def healthcheck():
+    if request.method == 'GET':
+        return "hello world", 200
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # if username != "test" or password != "test":
+    #     return jsonify({"msg": "Bad username or password"}), 401
+    data = User.query.filter_by(username=username).one_or_none()
+    print(data)
+
+    return data
+
+    # access_token = create_access_token(identity=username)
+    # return jsonify(access_token=access_token)
+
+
+@app.route("/is_logged_in", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	return jsonify(access_token=access_token)	
+
